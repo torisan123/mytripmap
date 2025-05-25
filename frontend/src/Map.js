fe4d52056e0ae8f9dda6072cbe970f197474638a
服部友理 <hattoriyuri@TORIS-MacBook-Air.local>
2025-05-25 20:24:17 +0900

    地図にピンを表示

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
-    width: '100%',
-    height: '400px'
+    width: '100vw',
+    height: '100vh'
 };
 
 const center = {
-    lat: 35.6895,
-    lng: 139.6917
+    lat: -15.769731249813606,
+    lng: -47.979260003588045
 }
 
-function Map() {
+const Map = () => {
+    const [destinations, setDestinations] = useState([
+        { id: 1, name: 'レンソイス・マラニャンセス国立公園', lat: -2.5276945169109597, lng: -43.02696388964509}
+    ]);
+
     return (
-        <LoadScript googleMapsApiKey="AIzaSyCkbnpInkfq-2RksWwSSueZhRKxSy7rKEQ">
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
             <GoogleMap
                 mapContainerStyle={containerStyle}
                 center={center}
-                zoom={10}
+                zoom={5}
             >
+                {destinations.map((place) => (
+                    <Marker key={place.id} position={{ lat:place.lat, lng:place.lng}} />                    
+                ))}
             </GoogleMap>
         </LoadScript>
     );
